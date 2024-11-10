640683fdc4b88bee59716a4456a393b0756e9813
@hexlet/cli <support@hexlet.io>
2024-11-11 01:59:48 +0300

    submit java-web-ru/crud

---
+++ java-web-ru/crud/src/main/java/exercise/util/NamedRoutes.java
@@ -0,0 +1,19 @@
+package exercise.util;
+
+public class NamedRoutes {
+
+    public static String rootPath() {
+        return "/";
+    }
+
+    // BEGIN
+    public static String postsPath() {
+        return "/posts";
+    }
+
+    public static String postPath(long id) {
+        return String.format("/posts/", id);
+    }
+
+    // END
+}
